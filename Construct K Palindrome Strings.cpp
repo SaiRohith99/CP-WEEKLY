b60e02fc58894ca47a6c08f1a59f714158c028e2
Rohith <47684745+SaiRohith99@users.noreply.github.com>
2020-04-04 23:41:33 +0530

    Create Construct K Palindrome Strings.cpp

---
+++ Construct K Palindrome Strings.cpp
@@ -0,0 +1,54 @@
+Construct K Palindrome Strings
+
+
+/*
+Given a string s and an integer k. You should construct k non-empty palindrome strings using all the characters in s.
+
+Return True if you can use all the characters in s to construct k palindrome strings or False otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+Example 4:
+
+Input: s = "yzyzyzyzyzyzyzy", k = 2
+Output: true
+Explanation: Simply you can put all z's in one string and all y's in the other string. Both strings will be palindrome.
+Example 5:
+
+Input: s = "cr", k = 7
+Output: false
+Explanation: We don't have enough characters in s to construct 7 palindromes.
+
+
+*/
+
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.size() < k) return false;
+        map<char, int> cnt;
+        for(auto i : s) cnt[i] += 1;
+        int hit = 0;
+        for(auto i : cnt){
+            if(i.second % 2 == 1) hit += 1;
+        }
+        if(hit <= k) return true;
+        return false;
+    }
+};
